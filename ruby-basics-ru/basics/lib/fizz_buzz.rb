30ab3124a51a125639b6d146329797db7932acb2
@hexlet/cli <support@hexlet.io>
2023-01-04 16:26:37 +0800

    submit ruby-basics-ru/basics

--- ruby-basics-ru/basics/lib/fizz_buzz.rb
+++ ruby-basics-ru/basics/lib/fizz_buzz.rb
@@ -4,16 +4,15 @@
 def fizz_buzz(start, stop)
   return nil if start > stop
 
-  (start.upto stop).to_a.map do |i|
-    if (i % 15).zero?
-      'FizzBuzz'
-    elsif (i % 3).zero?
-      'Fizz'
-    elsif (i % 5).zero?
-      'Buzz'
-    else
-      i
+  (start..stop)
+    .map do |i|
+      if (i % 15).zero? then 'FizzBuzz'
+      elsif (i % 3).zero? then 'Fizz'
+      elsif (i % 5).zero? then 'Buzz'
+      else
+        i
+      end
     end
-  end.join(' ')
+    .join(' ')
 end
 # END
