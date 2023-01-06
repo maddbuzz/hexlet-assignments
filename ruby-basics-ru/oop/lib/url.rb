31286324d34b8b172d3a2dab947158d12f6c9d87
@hexlet/cli <support@hexlet.io>
2023-01-06 17:23:19 +0800

    submit ruby-basics-ru/oop

--- ruby-basics-ru/oop/lib/url.rb
+++ ruby-basics-ru/oop/lib/url.rb
@@ -6,8 +6,16 @@ require 'uri'
 class Url
   def initialize(url)
     @uri = URI(url)
+    query_string = @uri.query || ''
+    @query_params =
+      query_string
+      .split('&')
+      .to_h do |pair|
+        k, v = pair.split('=')
+        [k.to_sym, v]
+      end
   end
-  attr_reader :uri
+  attr_reader :uri, :query_params
 
   extend Forwardable
   def_delegators :uri, :scheme, :host
@@ -15,18 +23,6 @@ class Url
   include Comparable
   def <=>(other)
     uri <=> other.uri
-  end
-
-  def query_params
-    query_string = uri.query
-    return {} if query_string.nil?
-
-    query_string
-      .split('&')
-      .to_h do |pair|
-        k, v = pair.split('=')
-        [k.to_sym, v]
-      end
   end
 
   def query_param(key, default = nil)
