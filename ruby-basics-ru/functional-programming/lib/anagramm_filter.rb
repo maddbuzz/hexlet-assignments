fca12cabbb8e2e14a232d934bb03b135b078b9b3
@hexlet/cli <support@hexlet.io>
2023-01-09 19:50:11 +0800

    submit ruby-basics-ru/functional-programming

--- ruby-basics-ru/functional-programming/lib/anagramm_filter.rb
+++ ruby-basics-ru/functional-programming/lib/anagramm_filter.rb
@@ -1,11 +1,18 @@
 # frozen_string_literal: true
 
 # BEGIN
+# def anagramm_filter(word, words)
+#   get_sorted_chars = ->(string) { string.chars.sort }
+#   word_sorted_chars = get_sorted_chars.call(word)
+#   words.filter do |w|
+#     get_sorted_chars.call(w) == word_sorted_chars
+#   end
+# end
 def anagramm_filter(word, words)
-  get_sorted_chars = ->(string) { string.chars.sort }
-  word_sorted_chars = get_sorted_chars.call(word)
+  size = word.size
+  tally = word.chars.tally
   words.filter do |w|
-    get_sorted_chars.call(w) == word_sorted_chars
+    size == w.size && tally == w.chars.tally
   end
 end
 # END
