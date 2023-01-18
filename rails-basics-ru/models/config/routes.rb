8ae2df632f02aa05a39d9b08769025032267c17a
@hexlet/cli <support@hexlet.io>
2023-01-18 14:39:36 +0800

    submit rails-basics-ru/models

--- rails-basics-ru/models/config/routes.rb
+++ rails-basics-ru/models/config/routes.rb
@@ -1,9 +1,12 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  root 'home#index'
   # BEGIN
-  
+  # root "articles#index"
+  # get "/articles", to: "articles#index"
+  # get "/articles/:id", to: "articles#show"
+  root "articles#index"
+  resources :articles  
   # END
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
--- rails-basics-ru/models/db/schema.rb
+++ rails-basics-ru/models/db/schema.rb
@@ -10,5 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_09_20_132659) do
+ActiveRecord::Schema[7.0].define(version: 2023_01_18_060839) do
+  create_table "articles", force: :cascade do |t|
+    t.string "title"
+    t.text "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
 end
