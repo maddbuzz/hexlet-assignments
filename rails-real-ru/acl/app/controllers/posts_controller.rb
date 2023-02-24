87614d9ca70e35a2f87cde541d25c2d159654748
@hexlet/cli <support@hexlet.io>
2023-02-24 14:26:54 +0800

    submit rails-real-ru/acl

--- rails-real-ru/acl/app/controllers/posts_controller.rb
+++ rails-real-ru/acl/app/controllers/posts_controller.rb
@@ -4,6 +4,67 @@ class PostsController < ApplicationController
   after_action :verify_authorized, except: %i[index show]
 
   # BEGIN
-  
+  def index
+    @posts = Post.all
+  end
+
+  def show
+    set_post
+  end
+
+  def new
+    raise Pundit::NotAuthorizedError, 'guest should raise error from new' unless signed_in?
+
+    @post = current_user.posts.build
+    authorize @post
+  end
+
+  def create
+    raise Pundit::NotAuthorizedError, 'guest cant create post' unless signed_in?
+
+    @post = current_user.posts.build(post_params)
+    authorize @post
+
+    if @post.save
+      redirect_to @post, notice: 'Post was successfully created.'
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+    set_post
+  end
+
+  def update
+    set_post
+
+    if @post.update(post_params)
+      redirect_to @post
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    set_post
+    @post.destroy
+    redirect_to posts_url, notice: 'Post was successfully destroyed.'
+  end
+
+  private
+
+  def set_post
+    @post = Post.find(params[:id])
+    authorize @post
+  end
+
+  def post_params
+    params.require(:post).permit(
+      :title,
+      :body,
+      :author_id
+    )
+  end
   # END
 end
--- rails-real-ru/acl/app/policies/post_policy.rb
+++ rails-real-ru/acl/app/policies/post_policy.rb
@@ -2,6 +2,32 @@
 
 class PostPolicy < ApplicationPolicy
   # BEGIN
-  
+  def index?
+    true
+  end
+
+  def show?
+    true
+  end
+
+  def create?
+    !user.nil?
+  end
+
+  def new?
+    create?
+  end
+
+  def update?
+    user&.admin? || record.author_id == user&.id
+  end
+
+  def edit?
+    update?
+  end
+
+  def destroy?
+    user&.admin?
+  end
   # END
 end
