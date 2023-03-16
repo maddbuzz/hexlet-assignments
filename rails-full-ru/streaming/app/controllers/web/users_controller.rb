5d138473e8ae8f814bacc805167f84e7dc0ee54c
@hexlet/cli <support@hexlet.io>
2023-03-16 17:51:04 +0800

    submit rails-full-ru/streaming

--- rails-full-ru/streaming/app/controllers/web/users_controller.rb
+++ rails-full-ru/streaming/app/controllers/web/users_controller.rb
@@ -51,6 +51,9 @@ class Web::UsersController < Web::ApplicationController
     redirect_to users_url, notice: t('success')
   end
 
+  # BEGIN
+
+  # Completed 200 OK in 30397ms (Views: 1.3ms | ActiveRecord: 703.8ms | Allocations: 23608985)
   def normal_csv
     respond_to do |format|
       format.csv do
@@ -60,25 +63,33 @@ class Web::UsersController < Web::ApplicationController
     end
   end
 
-  # BEGIN
+  # Completed 200 OK in 62869ms (ActiveRecord: 745.8ms | Allocations: 24802699)
   def stream_csv
-    # response.headers['Content-Type'] = 'text/event-stream'
+    response.headers['Last-Modified'] = Time.now.httpdate.to_s
     response.headers['Content-Type'] = 'text/csv'
-    response.headers["Last-Modified"] = Time.current.httpdate
     response.headers["Content-Disposition"] = "attachment;filename=report.csv"
 
     user_column_names = User.column_names
     response.stream.write "#{user_column_names.join(',')}\n"
-    User.all.each do |user|
-      # user_values = user.attributes.values_at(*user_column_names).join(',')
-      user_values = user.attributes.values.join(',')
-      response.stream.write "#{user_values}\n"
+    # User.all.each do |user|
+    User.find_each do |user|
+      response.stream.write "#{user.attributes.values.join(',')}\n"
     end
 
   ensure
     response.stream.close
   end
-  # END
+
+  # Hexlet: Completed 200 OK in 56707ms (ActiveRecord: 865.4ms | Allocations: 20903225)
+  # def stream_csv
+  #   response.headers['Last-Modified'] = Time.now.httpdate.to_s
+  #   send_stream(filename: 'users.csv') do |stream|
+  #     stream.write 'id,name,email,created_at,updated_at\n'
+  #     User.find_each do |user|
+  #       stream.write "#{user.id},#{user.name},#{user.email},#{user.created_at},#{user.updated_at}\n"
+  #     end
+  #   end
+  # end
 
   private
 
@@ -91,6 +102,8 @@ class Web::UsersController < Web::ApplicationController
       end
     end
   end
+
+  # END
 
   def user_params
     params.require(:user).permit(
